e7c9ca4514c1ce3b82616b615acc460410cc0007
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-18 14:13:14 +0545

    update main

--- AdminDashboard/src/main.jsx
+++ AdminDashboard/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { Store } from "./components/Redux/store.jsx";
+import { Store } from "./components/Redux/Store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
