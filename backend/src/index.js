994a09c927c326fb791c87a00a7e9e1b562806a4
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-18 16:55:10 +0545

    update frotend and admindashboard address

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,10 @@ const PORT = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    // origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://nexora-pralad1.vercel.app/", "https://nexora-awrw.vercel.app/"],
+
+
     credentials: true,
   })
 )
