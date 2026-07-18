5258b43eedbe05c4f686f2b30ce6126fa739b80c
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-18 17:00:31 +0545

    update address

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8000;
 app.use(
   cors({
     // origin: ["http://localhost:5173", "http://localhost:5174"],
-    origin: ["https://nexora-pralad1.vercel.app/", "https://nexora-awrw.vercel.app/"],
+    origin: ["https://nexora-pralad1.vercel.app", "https://nexora-awrw.vercel.app"],
 
 
     credentials: true,
