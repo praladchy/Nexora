5d72656d2295cad96304d47a3c16bf8668395b86
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-19 16:11:42 +0545

    update env

--- backend/src/config/email.config.js
+++ backend/src/config/email.config.js
@@ -1,28 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 import nodemailer from "nodemailer";
- 
 
 export const transporter = nodemailer.createTransport({
-  
-//  host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false, // MUST be false for 587
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_PASS, // App Password only
-    
-//   },
-
-
- host: process.env.BREVO_HOST,
-  port: Number(process.env.BREVO_PORT),
-  secure: true, // Use false with port 587
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // MUST be false for 587
   auth: {
-    user: process.env.BREVO_LOGIN,
-    pass: process.env.BREVO_SMTP_KEY,
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS, // App Password only
   },
-}
-
-);
- 
+});
