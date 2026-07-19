dfe3922ec404575a0c01e4329a2a0d8922001cbe
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-19 16:32:38 +0545

    test using  local frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,8 +21,8 @@ const PORT = process.env.PORT || 8000;
 
 app.use(
   cors({
-    // origin: ["http://localhost:5173", "http://localhost:5174"],
-    origin: ["https://nexora11.vercel.app", "https://nexora-39iv.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    // origin: ["https://nexora11.vercel.app", "https://nexora-39iv.vercel.app"],
 
 
     credentials: true,
