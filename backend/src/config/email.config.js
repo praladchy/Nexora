d0856934e9e6fcdc4ad621d97ad92379a1dfcbda
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-19 16:45:25 +0545

    update config

--- backend/src/config/email.config.js
+++ backend/src/config/email.config.js
@@ -4,8 +4,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // MUST be false for 587
+  port: 465,
+  secure: true, // MUST be false for 587
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS, // App Password only
