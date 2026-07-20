208d6079b42d50adce37e5e89a17a10c6ed90127
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-20 14:00:46 +0545

    update email

--- backend/src/utils/emailVerification.js
+++ backend/src/utils/emailVerification.js
@@ -1,19 +1,45 @@
-import { transporter } from "../config/email.config.js";
+// import { transporter } from "../config/email.config.js";
 
-export const sendEmail = async ({ email ,otp }) => {
-  console.log("mail asdvfg",email,otp);
+// export const sendEmail = async ({ email ,otp }) => {
+//   console.log("mail asdvfg",email,otp);
 
-  
-  return transporter.sendMail({
-    from: process.env.MAIL_FROM,
-    to:email,
-    subject: "Your OTP Code", 
+//   return transporter.sendMail({
+//     from: process.env.MAIL_FROM,
+//     to:email,
+//     subject: "Your OTP Code",
 
-    html: `
+//     html: `
+//         <h2>Email Verification</h2>
+//         <p>Your OTP is:</p>
+//         <h1>${otp}</h1>
+//         <p>Valid for 5 minutes</p>
+//       `,
+//   });
+// };
+
+import { BrevoClient } from "@getbrevo/brevo";
+
+const brevo = new BrevoClient({
+  apiKey: "your-api-key",
+  // timeoutInSeconds: 30,
+  // maxRetries: 3,
+});
+export const sendEmail = async ({ email, otp }) => {
+  try {
+    const result = await brevo.transactionalEmails.sendTransacEmail({
+      subject: "Hello from Nexora!, Your OTP Code",
+      htmlContent: `
         <h2>Email Verification</h2>
         <p>Your OTP is:</p>
         <h1>${otp}</h1>
-        <p>Valid for 5 minutes</p>
+        <p>Valid for 30 minutes</p>
       `,
-  });
+      sender: { email: process.env.BREVO_EMAIL, name: "Nexora" },
+      to: [{ email }],
+    });
+
+    console.log("Email sent. Message ID:", result.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
