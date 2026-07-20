e6637721bcbe10f02adbd7bbe6089eaa3ee810e1
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-20 19:25:56 +0545

    add resend config

--- backend/src/utils/emailVerification.js
+++ backend/src/utils/emailVerification.js
@@ -17,30 +17,71 @@
 //   });
 // };
 
-import { BrevoClient } from "@getbrevo/brevo";
+// import { BrevoClient } from "@getbrevo/brevo";
 
-const brevo = new BrevoClient({
-  apiKey: process.env.BREVO_API_KEY,
-  // timeoutInSeconds: 30,
-  // maxRetries: 3,
-});
+// const brevo = new BrevoClient({
+//   apiKey: process.env.BREVO_API_KEY,
+//   // timeoutInSeconds: 30,
+//   // maxRetries: 3,
+// });
+// export const sendEmail = async ({ email, otp }) => {
+//   console.log("mail asdvfg", process.env.BREVO_API_KEY, process.env.BREVO_GMAIL, email, otp);
+//   try {
+//     const result = await brevo.transactionalEmails.sendTransacEmail({
+//       subject: "Hello from Nexora!, Your OTP Code",
+//       htmlContent: `
+//         <h2>Email Verification</h2>
+//         <p>Your OTP is:</p>
+//         <h1>${otp}</h1>
+//         <p>Valid for 30 minutes</p>
+//       `,
+//       sender: { email: process.env.BREVO_EMAIL, name: "Nexora" },
+//       to: [{ email }],
+//     });
+
+//     console.log("Email sent. Message ID:", result.messageId);
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//   }
+// };
+
+
+
+import { Resend } from "resend";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async ({ email, otp }) => {
-  console.log("mail asdvfg", process.env.BREVO_API_KEY, process.env.BREVO_GMAIL, email, otp);
+  console.log(
+    "Sending email:",
+    process.env.RESEND_API_KEY,
+    email,
+    otp
+  );
+
   try {
-    const result = await brevo.transactionalEmails.sendTransacEmail({
-      subject: "Hello from Nexora!, Your OTP Code",
-      htmlContent: `
+    const { data, error } = await resend.emails.send({
+      from: process.env.RESEND_GMAIL, // Use this for testing
+      to: email,
+      subject: "Hello from Nexora! Your OTP Code",
+      html: `
         <h2>Email Verification</h2>
         <p>Your OTP is:</p>
         <h1>${otp}</h1>
         <p>Valid for 30 minutes</p>
       `,
-      sender: { email: process.env.BREVO_EMAIL, name: "Nexora" },
-      to: [{ email }],
     });
 
-    console.log("Email sent. Message ID:", result.messageId);
-  } catch (error) {
-    console.error("Error sending email:", error);
+    if (error) {
+      console.error("Resend Error:", error);
+      return;
+    }
+
+    console.log("Email sent:", data);
+  } catch (err) {
+    console.error("Error sending email:", err);
   }
 };
