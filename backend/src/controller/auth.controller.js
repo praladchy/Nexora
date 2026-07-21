390bee4b9f8616cb2b10e4956b4b652aebe69ca3
Pralad Chaudhary <praladchy028@gmail.com>
2026-07-21 22:05:57 +0545

    update

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -375,6 +375,7 @@ export const verifyOtp = async (req, res) => {
 export const refresh = async (req, res) => {
   try {
     const token = req.cookies.refreshToken;
+    console.log("Refresh Token:", token);
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const user = await User.findOne({ refreshToken: token }).populate(
