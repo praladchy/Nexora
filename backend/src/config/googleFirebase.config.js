fc97dd8d371360ee55f4a9d8aa76c4accf8ee092
Pralad Chaudhary <praladchy028@gmail.com>
2026-08-18 00:55:35 +0545

    update googleloginjson

--- backend/src/config/googleFirebase.config.js
+++ backend/src/config/googleFirebase.config.js
@@ -1,8 +1,19 @@
 import admin from "firebase-admin";
-import serviceAccount from "../service/googleLoginService.json" assert { type: "json" };
-
+const privateKey = process.env.private_key?.replace(/\\n/g, "\n");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key:privateKey,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain,
+  }),
 });
 
 export default admin;
