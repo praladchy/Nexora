3ceeb93e0b575f2f43c006c80e1eef1a8e83d8c6
Pralad Chaudhary <praladchy028@gmail.com>
2026-09-26 15:25:55 +0545

    update

--- frontend/src/components/CategorySlider/CategoryByParentId.jsx
+++ frontend/src/components/CategorySlider/CategoryByParentId.jsx
@@ -73,9 +73,10 @@ const pairedCategories = pairCategories(cate);
 
   return (
     <div className="mx-auto max-w-7xl px-4 py-10">
+      <h2 className="text-3xl font-semibold">Categories</h2>
       <Slider {...settings}>
         {pairedCategories.map((pair, index) => (
-          <div key={index} className="px-3 py-4">
+          <div key={index} className="md:px-3 py-4">
             <div className="flex flex-col gap-8">
               {pair.map((item) => (
                 <div
